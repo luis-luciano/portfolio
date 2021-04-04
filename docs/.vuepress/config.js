c0ed9422a20920f286b51257243d7771b875fdc7
Luis Luciano <alberto.rammss@gmail.com>
2021-04-04 17:12:24 -0500

    homepage

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Luis Luciano",
+	description: "Página personal",
+	themeConfig: {
+		nav: [
+			{ text: "Home", link: "/" },
+			{ text: "Portafolio", link: "/portafolio/" },
+			{ text: "Acerca de", link: "/acerca-de/" }
+		]
+	}
+};
---
+++ docs/README.md
@@ -0,0 +1,10 @@
+---
+home: true
+lang: es
+heroImage: /img/13295061.png
+heroText: Luis Alberto Luciano Morales
+tagline: Web Developer - Backend 💻
+actionText: Get Started →
+actionLink: /portafolio/
+footer: Copyright © 2021 Luis Luciano
+---
